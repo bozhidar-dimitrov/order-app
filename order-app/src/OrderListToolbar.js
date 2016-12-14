6dedecb1ea0ed33acf57ad21c67f6c86a74b811e
bozhidar-dimitrov <bozhidar.dimitrov@gmail.com>
2016-12-14 06:48:06 +0200

    Latest Versions

---
+++ order-app/src/OrderListToolbar.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+import muiThemeable from 'material-ui/styles/muiThemeable';
+
+import ToggleButton from './ToggleButton';
+import DatePicker from 'material-ui/DatePicker';
+import AdvancedComponent from "./AdvancedComponent"
+
+class OrderListToolbar extends AdvancedComponent {
+	static propTypes = {
+		id:React.PropTypes.string.isRequired,
+	};
+
+	constructor(props) {
+		super(props, "./OrderListToolbar-Default.css");
+	}
+
+	render() {
+		const normalButtonStyle = {
+			color:"rgba(150, 150, 150, 1)"
+		};
+
+		const toggleButtonStyle = {
+			color:this.props.muiTheme.palette.primary1Color
+		};
+
+		return (
+			<div id={this.props.id} className="order-list-toolbar">
+					<div className="button-bar">
+						<ToggleButton 
+							className="accepted-order-button"
+							isToggled={true} 
+							normal={{
+								label:"Accepted",
+								style:normalButtonStyle
+							}}
+							toggle= {{
+								style:toggleButtonStyle
+							}}/>
+						<ToggleButton 
+							className="ready-orders-button"  
+							isToggled={false}
+							normal={{
+								label:"Ready",
+								style:normalButtonStyle
+							}}
+							toggle= {{
+								style:toggleButtonStyle
+							}}
+							/>
+						<ToggleButton 
+							className="delivered-orders-button" 
+							isToggled={false}
+							normal={{
+								label:"Delivered",
+								style:normalButtonStyle
+							}}
+							toggle= {{
+								style:toggleButtonStyle
+							}}/>
+					</div>
+					<div className = "dateToolbar">
+						<DatePicker className="from-date-picker" hintText="From Date" />
+						<DatePicker className="to-date-picker" hintText="To Date" />
+					</div>
+				</div>
+		);
+	}
+}
+
+export default muiThemeable()(OrderListToolbar);
